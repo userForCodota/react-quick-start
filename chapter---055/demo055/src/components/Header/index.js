e916587e5215d476fc1ca9569bfb4221c18e737f
gust <2414566866@qq.com>
2024-12-15 23:22:58 +0800

    20241215_232258

--- chapter---055/demo055/src/components/Header/index.js
+++ chapter---055/demo055/src/components/Header/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
 
-export default class Footer extends Component {
+export default class Header extends Component {
     render() {
         return (
             <div>
-                <h1>this🤨is😋Footer😛Component🤪</h1>
+                <h1>this🤨is😋Header😛Component🤪</h1>
             </div>
         );
     }
--- chapter---055/demo055/src/components/Item/index.js
+++ chapter---055/demo055/src/components/Item/index.js
@@ -1,10 +1,10 @@
 import React, {Component} from "react";
 
-export default class Footer extends Component {
+export default class Item extends Component {
     render() {
         return (
             <div>
-                <h1>this🤨is😋Footer😛Component🤪</h1>
+                <h1>this🤨is😋Item😛Component🤪</h1>
             </div>
         );
     }
