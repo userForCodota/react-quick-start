16bbc0611a667d4d6b5d635e4e12df0aa92e6498
gust <2414566866@qq.com>
2024-12-16 01:07:46 +0800

    20241216_010746

--- chapter---055/demo055/src/components/Item/index.js
+++ chapter---055/demo055/src/components/Item/index.js
@@ -2,16 +2,26 @@ import React, {Component} from "react";
 import "./index.css";
 
 export default class Item extends Component {
+    state = {mouse: false}
+
+    handleOnMouseEnter = (event) => {
+        event.target.lastElementChild.style.display = "block";
+    }
+    handleOnMouseLeave = (event) => {
+        event.target.lastElementChild.style.display = "none";
+    }
+
     render() {
         const {id, name, done} = this.props;
+        const {mouse} = this.state;
         return (
             <div>
-                <li className="todo-li" li-data-id={"xxx" + id}>
+                <li className="todo-li" li-data-id={"xxx" + id} onMouseEnter={this.handleOnMouseEnter} onMouseLeave={this.handleOnMouseLeave}>
                     <label>
                         <input type="checkbox" defaultChecked={done}/>
                         <span>{name}</span>
                     </label>
-                    <button className="btn btn-danger" style={{display: "none"}}>删除</button>
+                    <button className="btn btn-danger" style={{display: mouse ? "block" : "none"}}>删除</button>
                 </li>
             </div>
         );
